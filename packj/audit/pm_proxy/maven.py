e8a42439a017081b3ac3c1e50502c14b06e0964e
shivaabhishek07 <shivaabhishek71@gmail.com>
2023-08-21 15:59:35 +0530

    Adds maven_pm abstract

--- packj/audit/pm_proxy/maven.py
+++ packj/audit/pm_proxy/maven.py
@@ -16,6 +16,44 @@ class MavenProxy(PackageManagerProxy):
         self.isolate_pkg_info = isolated_pkg_info
         self.metadata_format = 'pom'
         self.dep_format = 'json'
+        
+    def get_metadata(self, pkg_name, pkg_version=None):
+        # load cached metadata information
+        pkg_info_dir = self.get_pkg_info_dir(pkg_name=pkg_name)
+        if pkg_info_dir is not None:
+            metadata_fname = self.get_metadata_fname(pkg_name=pkg_name, pkg_version=pkg_version, fmt= self.metadata_format)
+            metadata_file = join(pkg_info_dir, metadata_fname)
+            if exists(metadata_file):
+                logging.warning("get_metadata: using cached metadata_file %s!", metadata_file)
+                if self.metadata_format == 'pom':
+                    return pkg_name, fromstring(open(metadata_file, 'r').read())
+                else:
+                    logging.error("get_metadata: output format %s is not supported!", self.metadata_format)
+                    return pkg_name, None
+        # Maven metadata is loaded in two steps
+        # First load names and versions. Then load the latest/specific version
+        try:
+            # Maven URL for specific version
+            # e.g, https://repo1.maven.org/maven2/com/google/protobuf/protobuf-java/3.6.1/protobuf-java-3.6.1.pom
+            metadata_url = "https://repo1.maven.org/maven2/%s" % self._get_pkg_path(
+                pkg_name=pkg_name, pkg_version=self._get_sanitized_version(pkg_name=pkg_name, pkg_version=pkg_version),
+                suffix="pom")
+            metadata_content = requests.request('GET', metadata_url)
+            pkg_info = fromstring(metadata_content.text)
+        except Exception as e:
+            logging.error("fail in get_metadata for pkg %s: %s, ignoring!", pkg_name, str(e))
+            return pkg_name, None
+        if pkg_info_dir is not None:
+            if not exists(pkg_info_dir):
+                os.makedirs(pkg_info_dir)
+            metadata_fname = self.get_metadata_fname(pkg_name=pkg_name, pkg_version=pkg_version,
+                                                     fmt=self.metadata_format)
+            metadata_file = join(pkg_info_dir, metadata_fname)
+            if self.metadata_format == 'pom':
+                open(metadata_file, 'w').write(metadata_content.text)
+            else:
+                logging.error("get_metadata: output format %s is not supported!", self.metadata_format)
+        return pkg_name, pkg_info
 
     def _get_versions_info(self, pkg_name):
         gid, aid = pkg_name.split('/')
@@ -43,41 +81,3 @@ class MavenProxy(PackageManagerProxy):
             return self.get_version(pkg_name=pkg_name)
         else:
             return pkg_version
-
-    def get_metadata(self, pkg_name, pkg_version=None):
-        # load cached metadata information
-        pkg_info_dir = self.get_pkg_info_dir(pkg_name=pkg_name)
-        if pkg_info_dir is not None:
-            metadata_fname = self.get_metadata_fname(pkg_name=pkg_name, pkg_version=pkg_version, fmt= self.metadata_format)
-            metadata_file = join(pkg_info_dir, metadata_fname)
-            if exists(metadata_file):
-                logging.warning("get_metadata: using cached metadata_file %s!", metadata_file)
-                if self.metadata_format == 'pom':
-                    return fromstring(open(metadata_file, 'r').read())
-                else:
-                    logging.error("get_metadata: output format %s is not supported!", self.metadata_format)
-                    return None
-        # Maven metadata is loaded in two steps
-        # First load names and versions. Then load the latest/specific version
-        try:
-            # Maven URL for specific version
-            # e.g, https://repo1.maven.org/maven2/com/google/protobuf/protobuf-java/3.6.1/protobuf-java-3.6.1.pom
-            metadata_url = "https://repo1.maven.org/maven2/%s" % self._get_pkg_path(
-                pkg_name=pkg_name, pkg_version=self._get_sanitized_version(pkg_name=pkg_name, pkg_version=pkg_version),
-                suffix="pom")
-            metadata_content = requests.request('GET', metadata_url)
-            pkg_info = fromstring(metadata_content.text)
-        except Exception as e:
-            logging.error("fail in get_metadata for pkg %s: %s, ignoring!", pkg_name, str(e))
-            return None
-        if pkg_info_dir is not None:
-            if not exists(pkg_info_dir):
-                os.makedirs(pkg_info_dir)
-            metadata_fname = self.get_metadata_fname(pkg_name=pkg_name, pkg_version=pkg_version,
-                                                     fmt=self.metadata_format)
-            metadata_file = join(pkg_info_dir, metadata_fname)
-            if self.metadata_format == 'pom':
-                open(metadata_file, 'w').write(metadata_content.text)
-            else:
-                logging.error("get_metadata: output format %s is not supported!", self.metadata_format)
-        return pkg_info
